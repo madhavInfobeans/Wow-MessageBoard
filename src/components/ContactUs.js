144db17fc46d5a56f541a9b0f37fba9c5fa0396c
madhavInfobeans <madhav.singh@infobeans.com>
2021-05-21 19:33:10 +0530

    Succesfully able to send  contact data to backend along with mail sent to the concern user

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -1,11 +1,60 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
 import "../css/contactus.css";
+import axios from "axios";
 
 const ContactUs = () => {
+  const history = useHistory();
+  const [inputData, setContact] = useState({
+    firstname: "",
+    lastname: "",
+    email: "",
+    mobile: "",
+    message: "",
+  });
+  const [file, setFile] = useState("");
+
+  const handleFiles = e => {
+    setFile(e.target.files[0]);
+  };
+
+  let name, value;
+  const handleInputs = e => {
+    name = e.target.name;
+    value = e.target.value;
+    setContact({ ...inputData, [name]: value });
+  };
+
+  const { firstname, lastname, email, mobile, message } = inputData;
+
+  const PostData = async e => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("firstname", firstname);
+    formData.append("lastname", lastname);
+    formData.append("email", email);
+    formData.append("mobile", mobile);
+    formData.append("message", message);
+    formData.append("attachment", file);
+
+    const res = await axios.post("http://localhost:4000/contact", formData);
+    const data = await res.data;
+    console.log(data);
+    if (res.status === 422 || !data) {
+      window.alert("Invalid Registration");
+    } else {
+      window.alert("Contact form submitted");
+    }
+  };
   return (
     <div className="container-contact">
       <div className="contact">
-        <form className="contact-form ">
+        <form
+          className="contact-form"
+          method="POST"
+          encType="multipart/form-data"
+          onSubmit={PostData}
+        >
           <span className="contact-form-title">Get in Touch</span>
           <div className="contact-input-field ">
             <label className="input-label">
@@ -13,9 +62,11 @@ const ContactUs = () => {
             </label>
             <input
               className="input-field"
-              id="name"
+              id="fname"
               type="string"
-              name="fname"
+              name="firstname"
+              value={inputData.firstname}
+              onChange={handleInputs}
               placeholder="Your FirstName"
             />
           </div>
@@ -27,7 +78,9 @@ const ContactUs = () => {
               className="input-field"
               id="name"
               type="textInput"
-              name="lname"
+              name="lastname"
+              value={inputData.lastname}
+              onChange={handleInputs}
               placeholder="Your LastName"
             />
           </div>
@@ -39,6 +92,8 @@ const ContactUs = () => {
               className="input-field"
               type="textInput"
               name="email"
+              value={inputData.email}
+              onChange={handleInputs}
               placeholder=" Enter Email"
             />
           </div>
@@ -51,6 +106,8 @@ const ContactUs = () => {
               id="mobile"
               type="textInput"
               name="mobile"
+              value={inputData.mobile}
+              onChange={handleInputs}
               placeholder="Contact"
             />
           </div>
@@ -59,11 +116,19 @@ const ContactUs = () => {
               type="textarea"
               className="input-field"
               name="message"
+              value={inputData.message}
+              onChange={handleInputs}
               placeholder="Any message..."
             />
           </div>
           <div className="contact-input-field ">
-            <input className="input-field" id="file" type="file" name="file" />
+            <input
+              className="input-field"
+              id="file"
+              onChange={handleFiles}
+              type="file"
+              name="attachment"
+            />
           </div>
           <div className="contact-form-btn">
             <input type="submit" className="form-btn" value="Send Email" />
